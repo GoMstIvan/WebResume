81281c1e4b844e9ca99dfd29711d1391796834f3
ivan.chen <ivan.chen@example.com>
2026-01-02 15:47:40 +0800

    20260102 154700

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/WebResume/',
   plugins: [
     react(),
     tailwindcss(),
